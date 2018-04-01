a349b80cf39af53115b5bd404c04232aa61aab76
Easy <easychen@qq.com>
2018-04-02 00:32:33 +0800

    change name

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 
 
-AppRegistry.registerComponent('rnapp', () => App);
+AppRegistry.registerComponent('FangTangResume', () => App);
